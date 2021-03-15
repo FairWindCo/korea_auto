4325e183ee04d2cb8be2a7e9ceacefb8faaa21a5
FairWind <sergey.manenok@gmail.com>
2021-03-15 17:25:58 +0200

    Create initial base template

--- list_auto/urls.py
+++ list_auto/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from list_auto.views import CarListView, CarDetailView
+from list_auto.views import CarListView, CarDetailView, index
 
 urlpatterns = [
-    path('', CarListView.as_view(), name='car_list_view'),
+    path('', index, name='index'),
+    path('cars', CarListView.as_view(), name='car_list_view'),
     path('car_<int:pk>/', CarDetailView.as_view(), name='car_detail')
 ]
--- list_auto/views.py
+++ list_auto/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView, DetailView
 
-from list_auto.models import Car
+from list_auto.models import Car, BodyType
 
 
 class CarListView(ListView):
@@ -15,3 +15,12 @@ class CarListView(ListView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'list_auto/car_detail_template.html'
+
+
+def index(request):
+    car_body = BodyType.objects.all()
+    cars = Car.objects.order_by('-created').order_by().all()[:5]
+    return render(request, 'list_auto/car_index_template.html', {
+        'car_body': car_body,
+        'car_list': cars,
+    })
