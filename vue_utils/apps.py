cf8b3c728973a0ae7377832da58e0e0d2b676fb4
FairWind <sergey.manenok@gmail.com>
2021-04-06 12:53:12 +0300

    Move utils classes to separate project vue_utils and add it to requirement

--- vue_utils/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VueUtilsConfig(AppConfig):
-    name = 'vue_utils'
--- vue_utils/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from vue_utils.views import view_test
-
-urlpatterns = [
-    path('test/', view_test, name='test_base_vue'),
-]
